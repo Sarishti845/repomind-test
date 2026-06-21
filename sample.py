463cd10b20d565ec810233263cc75062cb9aacf1
Sarishti845 <sarishti260@gmail.com>
2026-06-21 21:43:41 +0530

    test: add sample code with bugs

---
+++ sample.py
@@ -0,0 +1,7 @@
+﻿def calculate_discount(price, discount_percent):
+    final_price = price - (price * discount_percent / 100)
+    return final_price
+
+def get_user_data(user_id):
+    query = "SELECT * FROM users WHERE id = " + user_id
+    return query
