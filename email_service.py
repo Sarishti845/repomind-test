a6fd51c37ca64b4e29ce137c9824a9aaead01c98
Sarishti845 <sarishti260@gmail.com>
2026-06-29 22:54:52 +0530

    test: add email_service.py

---
+++ email_service.py
@@ -0,0 +1,10 @@
+﻿def send_email(user_email, subject, body):
+    if not user_email:
+        return False
+    message = subject + body
+    print("Sending to: " + user_email)
+    return True
+
+def get_user(user_id):
+    query = "SELECT * FROM users WHERE id = " + str(user_id)
+    return query
